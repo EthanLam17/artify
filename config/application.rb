f4b3b866daf728c89dd6c4e1e04fce992785ef4f
Ethan Lam <ethanlam0@yahoo.com>
2022-02-28 15:22:53 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 
 module Artify
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
