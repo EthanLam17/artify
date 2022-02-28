b3bd1e85213e714e2b2fc5e91b08250b1ac7d339
Ethan Lam <ethanlam0@yahoo.com>
2022-02-28 15:44:23 -0500

    Reinstall

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Artify
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
 
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
