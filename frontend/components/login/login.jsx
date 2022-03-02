54f772d39db6dd656c5c51b56ded7047afa90a98
Ethan Lam <ethanlam0@yahoo.com>
2022-03-02 17:52:28 -0500

    Edit navbar and side bar

--- frontend/components/login/login.jsx
+++ frontend/components/login/login.jsx
@@ -7,6 +7,7 @@ class Login extends React.Component {
         super(props)
         this.state = this.props.user;
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.demoSubmit = this.demoSubmit.bind(this);
     }
 
     handleInput(type) {
@@ -21,30 +22,46 @@ class Login extends React.Component {
             .then(() => (this.props.history.push('/home')))
     }
 
+    demoSubmit(e) {
+        e.preventDefault(e);
+        this.props.login({
+            email: 'demo',
+            password: 'password'
+        })
+        .then(() => (this.props.history.push('/home')))
+    }
+
 
     render() {
         return (
             <div className="login-cont">
-                <i className="fa-brands fa-spotify"></i>
-                <h1 className='logo'>Artify</h1>
-
-                <form className="login-form" onSubmit={this.handleSubmit}>
-                    <div className='form-comp'>To continue, log in to Artify</div>
-                    <label className="form-comp">What's your email?
-                        <input type="text" value={this.state.email} onChange={this.handleInput('email')} placeholder="Email address"/>
-                    </label>
-
-                    <label className="form-comp">Create a password
-                        <input type="password" value={this.state.password} onChange={this.handleInput('password')} placeholder="Password"/>
-                    </label>
-
-                    <button id="login-btn">LOG IN</button>
-                </form>
-
-                <div className="redirect">
-                    <div>Don't have an account?</div>
-                    <Link id="signup-redirect"to="/signup">SIGN UP FOR ARTIFY</Link>
+                <div className='header'>
+                    <i className="fa-brands fa-spotify"></i>
+                    <h1 className='logo'>Artify</h1>
                 </div>
+
+                <div className='form-comp'>To continue, log in to Artify</div>
+                    <div>
+                        <button className="demo-login" onClick={this.demoSubmit}>DEMO USER LOGIN</button>
+                    </div>
+
+
+                    <form className="login-form" onSubmit={this.handleSubmit}>
+                        <label className="form-comp">What's your email?
+                            <input type="text" value={this.state.email} onChange={this.handleInput('email')} placeholder="Email address"/>
+                        </label>
+
+                        <label className="form-comp">Create a password
+                            <input type="password" value={this.state.password} onChange={this.handleInput('password')} placeholder="Password"/>
+                        </label>
+
+                        <button id="login-btn">LOG IN</button>
+                    </form>
+
+                    <div className="redirect">
+                        <div>Don't have an account?</div>
+                        <Link id="signup-redirect"to="/signup">SIGN UP FOR ARTIFY</Link>
+                    </div>
 
             </div>
         )     
