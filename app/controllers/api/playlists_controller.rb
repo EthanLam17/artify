431ad7c7fc5ca45e37a8dd69472e67c68086b245
Ethan Lam <ethanlam0@yahoo.com>
2022-04-01 09:38:35 -0400

    Update main

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,41 +1,41 @@
 class Api::PlaylistsController < ApplicationController
 
-  def new 
-    @playlist = Playlist.new
-    render :new
-end
-
-def create
-    @playlist = Playlist.new(playlist_params)
-    if @playlist.save
-    else
-        render json: @playlist.errors.full_messages
+    def new 
+        @playlist = Playlist.new
+        render :new
     end
-end
 
-def index
-    @playlists = Playlist.all
-    if @playlists
-        render :index
-    else
-        render {'No Playlists'}
+    def create
+        @playlist = Playlist.new(playlist_params)
+        if @playlist.save
+        else
+            render json: @playlist.errors.full_messages
+        end
     end
-end
 
-def show
-    @playlist = Playlist.find(params[:id])
-    if @playlist
-        render :show
-    else
-        render {"No match"}
+    def index
+        @playlists = Playlist.all
+        if @playlists
+            render :index
+        else
+            render {'No Playlists'}
+        end
     end
-end
+
+    def show
+        @playlist = Playlist.find(params[:id])
+        if @playlist
+            render :show
+        else
+            render {"No match"}
+        end
+    end
 
 
 
-private 
-def playlist_params
-    params.require(:playlist).permit(:playlist_name, :user_id)
-end
+    private 
+    def playlist_params
+        params.require(:playlist).permit(:playlist_name, :user_id)
+    end
 
 end
